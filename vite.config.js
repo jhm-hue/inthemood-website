50bda2a5a284d9f03cb5d10001ad719ef6f0386c
jhm-hue <jakobhofstad@gmail.com>
2025-09-05 21:15:45 +0200

    Supabase client set up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true, // Sørger for at når man kjører npm run dev, så starter browser automatisk
+  },
+});
