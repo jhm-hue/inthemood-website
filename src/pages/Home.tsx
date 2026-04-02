4c9305eaf2c86d01d41c7e85a29cd02e6d404922
Ingvild Evjemo <ingvildevjemo@MacBook-Pro-3.local>
2026-04-02 18:10:04 +0200

    fjernet italic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,16 @@ const Home = () => {
       <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-wider text-white font-noto">
         INTHEMOOD STUDIO
       </h1>
+<<<<<<< HEAD
+      <h3 className="text-2xl md:text-3xl lg:text-4xl font-light text-white font-noto mt-4">
+      
+
+
+	<img src="bilde.jpeg">
+=======
       <h3 className="text-2xl md:text-3xl lg:text-4xl font-light text-white font-noto italic mt-4">
         <img src="bilde.jpeg" />
+>>>>>>> 871f591519e1d663f708878b454ea9242cddeddc
       </h3>
     </div>
   );
--- src/pages/Tech.tsx
+++ src/pages/Tech.tsx
@@ -3,6 +3,18 @@ import React from 'react';
 const Tech = () => {
   return (
     <div>
+<<<<<<< HEAD
+      <p className="text-l font-light text-white font-noto">
+     WE CAN DELIVER: <br></br>
+Synthetic universes. <br></br>
+Our default is to provide content for cinema and broadcast native resolution and fidelity.<br></br>
+When it comes to length and consistency, we are unlimited. <br></br>
+
+
+“The future will be better tomorrow” <br></br>
+Dan Quayle <br></br>
+        
+=======
       <p className="text-xl font-light text-white font-noto italic">
         WE CAN DELIVER: <br />
         Synthetic universes. <br />
@@ -11,6 +23,7 @@ const Tech = () => {
         <br />
         "The future will be better tomorrow" <br />
         Dan Quayle
+>>>>>>> 871f591519e1d663f708878b454ea9242cddeddc
       </p>
     </div>
   );
